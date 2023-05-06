a07f327e0030a2de088f6a373cc3f49d7680aa39
MAHI <56589615+maheshkumar121@users.noreply.github.com>
2023-05-06 12:27:09 +0530

    Update ArtJSfile.js

--- Codepen/ArtJSfile.js
+++ Codepen/ArtJSfile.js
@@ -12,64 +12,92 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    const isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    document.addEventListener('mousemove', (e) => {
+      if (!this.rotating) {
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
 
-    const startEvent = isTouchDevice ? 'touchstart' : 'mousedown';
-    const moveEvent = isTouchDevice ? 'touchmove' : 'mousemove';
-    const endEvent = isTouchDevice ? 'touchend' : 'mouseup';
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
 
-    document.addEventListener(moveEvent, (e) => {
+      const dirX = e.clientX - this.mouseTouchX;
+      const dirY = e.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = (180 * angle) / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
       if (this.holdingPaper) {
-        e.preventDefault();
-        const pageX = isTouchDevice ? e.touches[0].pageX : e.pageX;
-        const pageY = isTouchDevice ? e.touches[0].pageY : e.pageY;
-
         if (!this.rotating) {
-          const deltaX = pageX - this.touchX;
-          const deltaY = pageY - this.touchY;
-          this.currentPaperX = this.offsetX + deltaX;
-          this.currentPaperY = this.offsetY + deltaY;
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
         }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
 
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
     });
 
-    paper.addEventListener(startEvent, (e) => {
-      e.preventDefault();
+    paper.addEventListener('mousedown', (e) => {
       if (this.holdingPaper) return;
       this.holdingPaper = true;
 
       paper.style.zIndex = highestZ;
       highestZ += 1;
 
-      if (isTouchDevice) {
-        const touch = e.touches[0];
-        this.touchX = touch.pageX;
-        this.touchY = touch.pageY;
-      } else {
-        this.touchX = e.pageX;
-        this.touchY = e.pageY;
+      if (e.button === 0) {
+        this.mouseTouchX = this.mouseX;
+        this.mouseTouchY = this.mouseY;
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
       }
-
-      this.offsetX = this.currentPaperX - this.touchX;
-      this.offsetY = this.currentPaperY - this.touchY;
-
       if (e.button === 2) {
         this.rotating = true;
       }
     });
 
-    window.addEventListener(endEvent, () => {
+    paper.addEventListener('touchstart', (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      const touch = e.touches[0];
+      this.touchX = touch.clientX;
+      this.touchY = touch.clientY;
+      this.prevMouseX = this.touchX;
+      this.prevMouseY = this.touchY;
+
+      if (e.touches.length === 2) {
+        this.rotating = true;
+      }
+    });
+
+    window.addEventListener('mouseup', () => {
       this.holdingPaper = false;
       this.rotating = false;
     });
-  }
-}
 
-const papers = Array.from(document.querySelectorAll('.paper'));
+    window.addEventListener('touchend', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
 
-papers.forEach(paper => {
-  const p = new Paper();
-  p.init(paper);
-});
+    window.addEventListener('touchmove', (e) => {
+      if (this.holdingPaper) {
+        e.preventDefault();
+        const touch = e.touches[0];
+        this.mouseX = touch.clientX;
+        this.mouseY = touch.clientY;
+
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY
