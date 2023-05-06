f2f39876db4a0f5a6ccbd16883d5db373f5ec0f9
MAHI <56589615+maheshkumar121@users.noreply.github.com>
2023-05-06 11:31:03 +0530

    Update ArtJSfile.js

--- Codepen/ArtJSfile.js
+++ Codepen/ArtJSfile.js
@@ -16,16 +16,14 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    const isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-
-    const startEvent = isTouchDevice ? 'touchstart' : 'mousedown';
-    const moveEvent = isTouchDevice ? 'touchmove' : 'mousemove';
-    const endEvent = isTouchDevice ? 'touchend' : 'mouseup';
+    const startEvent = 'mousedown';
+    const moveEvent = 'mousemove';
+    const endEvent = 'mouseup';
 
     document.addEventListener(moveEvent, (e) => {
       if (!this.rotating) {
-        this.mouseX = isTouchDevice ? e.touches[0].clientX : e.clientX;
-        this.mouseY = isTouchDevice ? e.touches[0].clientY : e.clientY;
+        this.mouseX = e.clientX;
+        this.mouseY = e.clientY;
 
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
@@ -63,19 +61,10 @@ class Paper {
       paper.style.zIndex = highestZ;
       highestZ += 1;
 
-      if (isTouchDevice) {
-        const touch = e.touches[0];
-        this.touchX = touch.clientX;
-        this.touchY = touch.clientY;
-        this.prevMouseX = this.touchX;
-        this.prevMouseY = this.touchY;
-        e.preventDefault(); // Prevent default touchmove behavior
-      } else {
-        this.touchX = this.mouseX;
-        this.touchY = this.mouseY;
-        this.prevMouseX = this.mouseX;
-        this.prevMouseY = this.mouseY;
-      }
+      this.touchX = e.clientX;
+      this.touchY = e.clientY;
+      this.prevMouseX = this.touchX;
+      this.prevMouseY = this.touchY;
 
       if (e.button === 2) {
         this.rotating = true;
@@ -86,14 +75,74 @@ class Paper {
       this.holdingPaper = false;
       this.rotating = false;
     });
+
+    // Touch events
+    paper.addEventListener('touchstart', (e) => {
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      const touch = e.touches[0];
+      this.touchX = touch.clientX;
+      this.touchY = touch.clientY;
+      this.prevMouseX = this.touchX;
+      this.prevMouseY = this.touchY;
+
+      e.preventDefault(); // Prevent default touchmove behavior
+
+      if (e.touches.length === 2) {
+        this.rotating = true;
+      }
+    });
+
+    window.addEventListener('touchmove', (e) => {
+      if (!this.rotating) {
+        const touch = e.touches[0];
+        this.mouseX = touch.clientX;
+        this.mouseY = touch.clientY;
+
+        this.velX = this.mouseX - this.prevMouseX;
+                this.velY = this.mouseY - this.prevMouseY;
+      }
+
+      const dirX = this.mouseX - this.touchX;
+      const dirY = this.mouseY - this.touchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = 180 * angle / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
+      if (this.holdingPaper) {
+        if (!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    });
+
+    window.addEventListener('touchend', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const papers = Array.from(document.querySelectorAll('.paper'));
+const papers = Array.from(document.querySelectorAll('.paper'));
 
-  papers.forEach(paper => {
-    const p = new Paper();
-    p.init(paper);
-  });
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
 });
+
